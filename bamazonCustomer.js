67bbe3966bce36316dd757e1595892aa3f43a177
chbbs231 <atrobles231@gmail.com>
2019-11-02 01:10:09 -0700

    table working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 const inquirer = ('inquirer')
 const mysql = require('mysql2')
-const table = require('cli-table2')
+const Table = require('cli-table2')
 
 
 const connection = mysql.createConnection({
@@ -12,24 +12,27 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-var display = function(){
-    connection.query("Select * From bamazon_db.products", function (err, res){
+var display = function() {
+    connection.query('Select * From bamazon_db.products', function (err, res){
         if (err) throw err;
-        console.log("------------------------")
-        console.log(" Hi! Welcome to Bamazon ")
-        console.log("------------------------")
-        console.log("")
+        console.log("------------------------");
+        console.log(" Hi! Welcome to Bamazon ");
+        console.log("------------------------");
+        console.log("");
         
-    })
-    var table = new Table({
-        head: ["Item-Id", "Product_Name","Department_Name", "Price", "Stock_Quantity"],
+    const table = new Table({
+        head: ["Item Id", "Product Name","Department Name", "Price", "Stock Quantity"],
         colWidths: [30, 30, 20, 20,20],
-        colAlign: ["center", "left", "right"]
+        colAligns: ["center", "left", "right"],
+        style: {
+            head: ["aqua"],
+            compact: true
+        }
     });    
     for (let i = 0; i <res.length; i++){
-        table.push(res[i].Item-Id, res[i].Product_Name,res[i].Department_Name, res[i].Price, res[i].Stock_Quantity)
-    }
+        table.push([res[i].Id, res[i].Product_Name,res[i].Department_Name, res[i].Price, res[i].Stock_Quantity])}
     console.log(table.toString());
     console.log("")
+     })
 }
-    display()
+    display();
